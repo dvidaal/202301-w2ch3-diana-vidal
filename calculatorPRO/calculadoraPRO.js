2af23048584f2337e86442808fa3937c71d81416
Diana Vidal <dvidaal@gmail.com>
2023-01-18 20:17:35 +0100

    add calculatorPRO.js

---
+++ calculatorPRO/calculadoraPRO.js
@@ -0,0 +1,72 @@
+let resultadosObtenidos = [];
+let numerosIntroducidos = [];
+
+function iniciamosCalculadoraPRO() {
+  
+  const introducirNum = () => {
+    let datosObtenidos = [];
+    let datos;
+   do{
+     
+    datos = Number(prompt("Introduce un número para empezar a usar la CalculadoraPRO."));
+    if(isNaN(datos)){
+      alert("Debes introducir números NO letras");
+      return introducirNum();
+    }
+     if (datosObtenidos.length < 1 && datos === 0) {
+      alert("Introduce mínimo un número para poder usar la CalculadoraPRO.");
+      return introducirNum();
+    }
+    if(datos !== 0){
+      datosObtenidos.push(datos);
+    }
+   }
+   while (datos !== 0);
+   return datosObtenidos;
+  }
+  numerosIntroducidos = introducirNum();
+
+
+  const operacionesCalculadora = () => {
+    let operaciones = [];
+    if (numerosIntroducidos.length > 1) {
+      const suma = numerosIntroducidos.reduce((valorInicial, segundoValor) => valorInicial + segundoValor).toFixed(3);
+      const resta = numerosIntroducidos.reduce((valorInicial, segundoValor) => valorInicial - segundoValor).toFixed(3);
+      const multiplicacion = numerosIntroducidos.reduce((valorInicial, segundoValor) => valorInicial * segundoValor).toFixed(3);
+      const division = numerosIntroducidos.reduce((valorInicial, segundoValor) => valorInicial / segundoValor).toFixed(3);
+
+
+
+      operaciones.push('La suma es: ' + suma + ' La resta es: ' + resta + ' La multiplicación es: ' + multiplicacion + ' La división es: ' + division);
+      return operaciones;
+    }else{
+      
+      const raiz = Math.sqrt(numerosIntroducidos[0]);
+      operaciones.push(raiz.toFixed(3));
+      return operaciones;
+    }
+  }
+  resultadosObtenidos = operacionesCalculadora();
+
+
+
+  const mostramosResultadosOperaciones = () => {
+    
+    if (numerosIntroducidos.length = 2) {
+      alert("Los resultados son: " + resultadosObtenidos);
+    }
+  }
+  mostramosResultadosOperaciones();
+
+
+  const repetimosProceso = () => {
+    if (confirm("Quieres volver hacer calculos?")) {
+      return iniciamosCalculadoraPRO();
+    } else {
+      alert("Gracias por usar nuestra calculadora PRO.");
+    }
+  }
+  repetimosProceso();
+}
+iniciamosCalculadoraPRO();
+
